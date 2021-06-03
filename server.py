d6c5ed3695be55785e007584c245985926e0f064
masanori kobayashi <masanorikobayashi@masanorinoMacBook-Pro.local>
2021-06-03 19:36:04 +0900

    add skeleton

--- server.py
+++ server.py
@@ -1,15 +1,20 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+import os
 import tornado.ioloop
 import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.render("index.html")
 
 application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+        (r"/", MainHandler),
+    ],
+    template_path=os.path.join(os.getcwd(),  "templates"),
+    static_path=os.path.join(os.getcwd(),  "static"),
+)
 
 if __name__ == "__main__":
     application.listen(5000)
